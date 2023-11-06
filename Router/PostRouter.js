6960fee0f1f6742ca7d3aed1d8f3601fd27bf037
Imran <imran.mia@portonics.com>
2023-11-06 22:23:20 +0600

    post update

--- Router/PostRouter.js
+++ Router/PostRouter.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { createPost, getPosts } = require('../Controller/PostController');
+const { createPost, getPosts, updatePost } = require('../Controller/PostController');
 const authMiddleware = require('../Middleware/AuthMiddleware');
 
 router.post('/createPost', authMiddleware, createPost);
 router.get('/getPosts', authMiddleware, getPosts);
+router.put('/updatePost', authMiddleware, updatePost);
 
 module.exports = router
