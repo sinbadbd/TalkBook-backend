e6f59dc7ea767edd4bc1427968ad54c65e398b29
Imran <imran.mia@portonics.com>
2024-01-06 22:59:14 +0600

    user proile

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -1,11 +1,11 @@
 const User = require('../Model/User');
 
-const getUser = async(req, res) => {
+const getUser = async (req, res) => {
     try {
 
         const user = await User.findById(req.params.id).select("-password")
             .populate("followers following", "-password")
-        
+
         if (!user) {
             res.status(404).send({
                 code: 404,
@@ -21,7 +21,7 @@ const getUser = async(req, res) => {
             user: user
         })
 
-        
+
     } catch (error) {
         console.log(error)
         return res.status(500).send({
@@ -32,7 +32,29 @@ const getUser = async(req, res) => {
         })
     }
 }
-
+const updateUser = async (req, res) => {
+    try {
+        const { fullname, coverPhoto, mobile, address, story, website, bio } = req.body
+      const user =  await User.findOneAndUpdate({ _id: req.user._id }, {
+            fullname, coverPhoto, mobile, address, story, website, bio
+        })
+        res.status(200).send({
+            code: 200,
+            success: true,
+            message: "Successully updated user data",
+            user: user
+        }) 
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send({
+            code: 500,
+            success: false,
+            message: "Something went wrong! Please try again!",
+            error: error.message
+        })
+    }
+}
 module.exports = {
-    getUser
+    getUser,
+    updateUser
 }
